b9943fb6797524600b6a45fdac461e3ffb25d37c
Anwar Mujeeb <ibnmujeeb2003@gmail.com>
2023-11-29 13:24:06 -0800

    clean up with cloud function tools and model weights

---
+++ cloud_function_util/deploy.py
@@ -0,0 +1,42 @@
+import joblib
+import numpy as np
+import pickle
+from flask import request
+from google.cloud import storage
+
+storage_client = storage.Client()
+bucket = storage_client.get_bucket("frontal_ai_model") # remember to change the bucket name
+modelBlob = bucket.blob("model/model_2.pkl")
+scalerBlob = bucket.blob("model/preprocessor.pkl")
+modelBlob.download_to_filename("/tmp/model_2.pkl")
+scalerBlob.download_to_filename("/tmp/preprocessor.pkl")
+model = joblib.load("/tmp/model_2.pkl")
+preprocessor = pickle.load(open('/tmp/preprocessor.pkl', 'rb'))
+
+
+def predict(request):
+     # Extract data from request
+    request_json = request.get_json()
+    
+    subject = request_json['class']
+    print("subject received " + str(subject))
+
+    problems = request_json['problems']
+    print("problems received " + str(problems))
+
+    priority = request_json['priority']
+    print("priority received " + str(priority))
+
+    row_values = [subject, problems, priority]
+    print("creating numpy array")
+
+    x_new = np.array(row_values).reshape(1,-1)
+
+    # Preprocess the data
+    processed_data = preprocessor.transform(x_new)
+
+     # Make a prediction
+    prediction = model.predict(processed_data)
+
+    # Return the prediction
+    return str(prediction)
---
+++ cloud_function_util/entrypoint.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import pickle
+import numpy as np
+from google.cloud import storage
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+def load_model_and_preprocessor():
+    bucket_name = 'frontal_ai_model'
+    model_path = 'models/my_model'
+    preprocessor_path = 'models/preprocessor.pkl'
+
+    download_blob(bucket_name, model_path, '/tmp/my_model')
+    download_blob(bucket_name, preprocessor_path, '/tmp/preprocessor.pkl')
+
+    # Load the TensorFlow model
+    model = tf.keras.models.load_model('/tmp/my_model')
+    # Load the preprocessor
+    preprocessor = pickle.load(open('/tmp/preprocessor.pkl', 'rb'))
+
+    return model, preprocessor
+
+model, preprocessor = load_model_and_preprocessor()
+
+def predict(request):
+    # Extract data from request
+    request_json = request.get_json()
+    
+    subject = request_json['class']
+    print("subject received " + str(subject))
+
+    problems = request_json['problems']
+    print("problems received " + str(problems))
+
+    priority = request_json['priority']
+    print("priority received " + str(priority))
+
+    row_values = [subject, problems, priority]
+    print("creating numpy array")
+
+    x_new = np.array(row_values).reshape(1,-1)
+
+    # Preprocess the data
+    processed_data = preprocessor.transform(x_new)
+
+     # Make a prediction
+    prediction = model.predict(processed_data)
+
+    # Return the prediction
+    return str(prediction.tolist())
+
